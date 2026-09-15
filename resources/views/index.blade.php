b753192e9936e7f3391a2d5c25c01a3496516075
fantomagg <eg112468@gmail.com>
2026-09-15 11:12:54 +0500

    Добавил 2 шаблона main, header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title')
+    Welcome
+@endsection
+
+@section('content')
+@include('layouts.header')
+
+
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <header>
+        <a class="logo"><img class="logo-png" alt="logo" src="{{ asset('logo.png') }}"></a>
+    </header>
+@endsection
+
